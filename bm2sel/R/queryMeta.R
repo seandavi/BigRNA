6066bbe00aa2c44be4eda0c3266041119bff8c23
Vince Carey <stvjc@channing.harvard.edu>
2018-03-08 09:46:34 -0500

    additional support for metadata in mongodb

---
+++ bm2sel/R/queryMeta.R
@@ -0,0 +1,21 @@
+#' arbitrary slice of metadata in public mongodb
+#' @import mongolite
+#' @examples
+#' m1 = humrnaMeta()
+#' m1$count()
+#' m1$find(limit=1)
+#' @export
+humrnaMeta = function()
+  mongo(url="mongodb://mongo.channingremotedata.org:27017", db="minihumrna",
+     collection="humrna")
+
+#' helper for regex construction for mongolite find
+#' @importFrom rjson toJSON
+#' @param fld character(1) field name
+#' @param patt character(1) regex string
+#' @param opt character(1) option string, 'i' means case-insensitive, an option seems obligatory
+#' @examples
+#' makeRegex()
+#' @export
+makeRegex = function(fld="study_center", patt="UT", opt="i")
+  toJSON(sprintf("{\"%s\":{\"$regex\":\"%s\",\"$options\":\"%s\"}}", fld, patt, opt))
